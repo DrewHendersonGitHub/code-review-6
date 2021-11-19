240d00806e9ff15a24f0f529d611de67ed568dc3
Drew Henderson <drewhendersonemail@gmail.com>
2021-11-19 11:12:51 -0800

    added code to convert between two currencies

--- src/convert.js
+++ src/convert.js
@@ -1,6 +1,6 @@
 export default class ConvertService {
   static convert(base) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${base}`)
+    return fetch(`https://v6.exchangerate-api.com/v6/58a719a54201bb47c9d205a9/latest/${base}`)
     .then(function(response) {
       if (!response.ok) {
         throw Error(response.statusText);
--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+import ConvertService from './convert.js';
+
+function displayResults(response, base, newCurrency, amount) {
+  if (response.conversion_rates[newCurrency]) {
+    return `<p>${amount} ${base} is equal to ${response.conversion_rates[newCurrency] * amount} ${newCurrency}.</p>`;
+  }
+  return `<p>${newCurrency} not found.</p>`;
+}
+
+function displayError(error) {
+  $('show-errors').text(`${error}`);
+}
 
 $(document).ready(function() {
   $('#convert').click(function() {
-    ConvertService.convert("USD")
+    ConvertService.convert(base.toUpperCase())
     .then(function(convertResponse) {
       if (convertResponse instanceof Error) {
         throw Error(`ExchangeRate-API error: ${convertResponse.message}`);
       }
-      displayResults(convertResponse);
+      displayResults(convertResponse, base.toUpperCase(), newCurrency.toUpperCase(), amount);
     })
     .catch(function(error) {
       displayError(error.message);
