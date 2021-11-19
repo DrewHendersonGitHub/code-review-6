634f95c8a9fd3aa715b42800ab91bf6d59fe236d
Drew Henderson <drewhendersonemail@gmail.com>
2021-11-19 10:18:49 -0800

    added exchangerate api call

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+
+$(document).ready(function() {
+  $('#convert').click(function() {
+    ConvertService.convert("USD")
+    .then(function(convertResponse) {
+      if (convertResponse instanceof Error) {
+        throw Error(`ExchangeRate-API error: ${convertResponse.message}`);
+      }
+      displayResults(convertResponse);
+    })
+    .catch(function(error) {
+      displayError(error.message);
+    });
+  });
+});
